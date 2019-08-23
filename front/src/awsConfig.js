30701fd87c71185ba71ed1cbef8b730208e19eca
yoshiki-0428 <rrrtcdish2@gmail.com>
2019-08-23 18:04:02 +0900

    AWS認証情報の追加

---
+++ front/src/awsConfig.js
@@ -0,0 +1,6 @@
+export default {
+  Region: 'us-east-1',
+  UserPoolId: 'us-east-1_ff1SlJRo2',
+  ClientId: 'iir4mqqbu3bo8uhmd7g46a07a',
+  IdentityPoolId: 'us-east-1:d2c732a4-b954-44c4-b84a-a54b923b834c'
+}
