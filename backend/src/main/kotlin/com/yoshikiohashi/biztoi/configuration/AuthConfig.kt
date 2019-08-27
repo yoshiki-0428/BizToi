da0050d3c70ead6bbaf803392890b7797eb4f05e
Yoshiki Ohashi <rrrtcdish2@gmail.com>
2019-08-27 23:26:41 +0900

    API Documentを読めるように修正

--- backend/src/main/kotlin/com/yoshikiohashi/biztoi/configuration/AuthConfig.kt
+++ backend/src/main/kotlin/com/yoshikiohashi/biztoi/configuration/AuthConfig.kt
@@ -6,6 +6,7 @@ import com.nimbusds.jwt.proc.ConfigurableJWTProcessor
 import com.yoshikiohashi.biztoi.service.AuthService
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.builders.WebSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.config.http.SessionCreationPolicy
@@ -36,6 +37,18 @@ class AuthConfig(
         http
                 .cors()
                 .configurationSource(corsConfigurationSource())
+    }
+
+    override fun configure(web: WebSecurity) {
+        web
+                .ignoring()
+                .antMatchers(
+                        "/v2/api-docs",
+                        "/configuration/ui",
+                        "/swagger-resources/**",
+                        "/configuration/security",
+                        "/swagger-ui.html",
+                        "/webjars/**")
     }
 
     /**
