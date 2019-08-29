caca96593dcf33c3385dee922b07d3c3dc45f802
yoshiki-0428 <rrrtcdish2@gmail.com>
2019-08-29 19:06:52 +0900

    WIP いみわからん

--- backend/src/main/kotlin/com/yoshikiohashi/biztoi/configuration/AuthConfig.kt
+++ backend/src/main/kotlin/com/yoshikiohashi/biztoi/configuration/AuthConfig.kt
@@ -1,51 +1,37 @@
 package com.yoshikiohashi.biztoi.configuration
 
-import com.yoshikiohashi.biztoi.filter.AuthFilter
-import com.nimbusds.jose.proc.SecurityContext
-import com.nimbusds.jwt.proc.ConfigurableJWTProcessor
-import com.yoshikiohashi.biztoi.service.AuthService
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.security.authentication.ReactiveAuthenticationManager
-import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
-import org.springframework.security.config.web.server.SecurityWebFiltersOrder
 import org.springframework.security.config.web.server.ServerHttpSecurity
 import org.springframework.security.web.server.SecurityWebFilterChain
-import org.springframework.web.cors.reactive.CorsWebFilter
-import org.springframework.web.cors.CorsConfiguration
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource
-import org.springframework.web.cors.reactive.CorsConfigurationSource
-import org.springframework.security.web.server.context.WebSessionServerSecurityContextRepository
-import org.springframework.security.web.server.util.matcher.ServerWebExchangeMatchers
-import org.springframework.security.web.server.util.matcher.NegatedServerWebExchangeMatcher
-import org.springframework.security.web.server.authentication.AuthenticationWebFilter
-
-
 
 /**
  * Configuration for web security
  */
 @EnableWebFluxSecurity
-@EnableReactiveMethodSecurity
 class AuthConfig(
-        val processor: ConfigurableJWTProcessor<SecurityContext>,
-        val authService: AuthService
+        val authenticationManager: AuthenticationManager,
+        val securityContextRepository: SecurityContextRepository
 ) {
-    @Bean
-    fun springSecurityFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain {
-        http
-                .addFilterAt(AuthFilter(processor, authService), SecurityWebFiltersOrder.AUTHENTICATION)
-                .authorizeExchange()
-                .pathMatchers("/api/auth/**").permitAll()
-                .anyExchange().authenticated()
 
+    @Bean
+    fun securityFilterChain(
+            http: ServerHttpSecurity
+    ): SecurityWebFilterChain {
         http
                 .httpBasic().disable()
                 .formLogin().disable()
                 .csrf().disable()
                 .logout().disable()
+
+        http.authenticationManager(this.authenticationManager)
+        http.securityContextRepository(this.securityContextRepository)
+        http
+//                .addFilterAt(AuthFilter(processor, authService), SecurityWebFiltersOrder.AUTHENTICATION)
+                .authorizeExchange().pathMatchers("/api/auth/**").permitAll()
+        http.authorizeExchange().anyExchange().authenticated()
+
         return http.build()
     }
 }
