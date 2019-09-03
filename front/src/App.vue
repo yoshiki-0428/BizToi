563f0446aa0260a52e516b1dbbe97150aadf40cc
ma-ryu <maryu-januar@gmail.com>
2019-09-03 20:07:27 +0900

    書き方微修正

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,30 +1,34 @@
+<style>
+li {
+  list-style: none;
+}
+</style>
+
 <template>
-  <v-app>
-  <Header></Header>
-  <v-flex xs6 md12>
-      <v-spacer></v-spacer>
-      <Main1></Main1>
-      <Main2></Main2>
+<v-app>
+  </main-header>
+  <v-flex xs12 md12>
+    <v-spacer></v-spacer>
+    <Main1></Main1>
+    <Main2></Main2>
     <v-content>
       <router-view />
     </v-content>
-    </v-flex>
-  </v-app>
-
+  </v-flex>
+</v-app>
 </template>
 
 <script>
-import Header from './components/Header'
+import MainHeader from './components/MainHeader'
 import Main1 from './components/Main1'
 import Main2 from './components/Main2'
 
 export default {
   name: "App",
-  components : {  Header,Main1,Main2  }
+  components: {
+    MainHeader,
+    Main1,
+    Main2
   }
-</script>
-<style>
-li{
-list-style:none;
 }
-</style>
+</script>
---
+++ front/src/components/MainHeader.vue
@@ -0,0 +1,12 @@
+<template>
+<v-toolbar>
+    <v-toolbar-title><router-link tag="li" id="home-nav" to="/" exact><a>ビジトイ！</a></router-link></v-toolbar-title>
+     <div class="flex-grow-1"></div>
+    <v-toolbar-items>
+      <v-btn text><router-link tag="li" id="home-nav" to="/About" exact><a>ABOUT</a></router-link></v-btn>
+      <v-btn text><router-link tag="li" id="home-nav" to="/Lesson" exact><a>LESSON</a></router-link></v-btn>
+      <v-btn text><router-link tag="li" id="home-nav" to="/Company" exact><a>COMPANY</a></router-link></v-btn>
+      <v-btn text><router-link tag="li" id="home-nav" to="/Contact" exact><a>CONTACT</a></router-link></v-btn>
+    </v-toolbar-items>
+  </v-toolbar>
+</template>
