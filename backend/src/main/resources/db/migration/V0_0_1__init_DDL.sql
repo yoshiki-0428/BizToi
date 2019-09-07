8c8ac158f3a015b1a39af43a1518c38403217819
Yoshiki Ohashi <rrrtcdish2@gmail.com>
2019-09-07 21:49:51 +0900

    DDLの作成、FlywayUtilの削除

--- backend/src/main/resources/db/migration/V0_0_1__init_DDL.sql
+++ backend/src/main/resources/db/migration/V0_0_1__init_DDL.sql
@@ -4,3 +4,48 @@ create table user (
     ACCESS_TOKEN text,
     REFRESH_TOKEN text
 ) charset=utf8;
+
+create table toi (
+    ID int primary key,
+    USER_ID int,
+    BOOK_ID int,
+    PICTURE_URL varchar(255),
+    TITLE varchar(255),
+    DETAIL varchar(255),
+    CONTENT varchar(255),
+    PUBLIC_FLG varchar(1),
+    INSERT_DATE date,
+    UPDATED_DATE date,
+    DELETED_DATE date
+) charset=utf8;
+
+create table question (
+    ID int primary key,
+    TOI_ID int,
+    TITLE varchar(255),
+    DETAIL varchar(255),
+    REQUIRED varchar(1),
+    ORDER_ID int,
+    INSERT_DATE date,
+    UPDATED_DATE date,
+    DELETED_DATE date
+) charset=utf8;
+
+create table answer (
+    ID int primary key,
+    ISBN varchar(13),
+    PICTURE_URL varchar(255),
+    TITLE varchar(255),
+    DETAIL varchar(255),
+    LINK_URL varchar(255)
+) charset=utf8;
+
+create table talk (
+    ID int primary key,
+    TOI_ID int,
+    USER_ID int,
+    TALK_ID int,
+    CONTENT varchar(255),
+    INSERT_DATE date,
+    DELETED_DATE date
+)
