c42bbe5bee94311c54dec5de6877cd13b2b639c5
ma-ryu <maryu.januar@gmail.com>
2019-09-10 22:40:39 +0900

    検索フォーム作成中 CSS未着手

--- front/src/views/Contact.vue
+++ front/src/views/Contact.vue
@@ -1,6 +1,6 @@
 <template>
   <v-container>
-    <h1>contact</h1>
+    <h1>CONTACT</h1>
     <router-link to="/">Go to Home</router-link>
   </v-container>
 </template>
--- front/src/views/Lesson.vue
+++ front/src/views/Lesson.vue
@@ -2,5 +2,16 @@
   <v-container>
     <h1>LESSON</h1>
     <router-link to="/">Go to Home</router-link>
+    <books-search />
   </v-container>
 </template>
+
+<script>
+import BooksSearch from "../components/BooksSearch";
+
+export default {
+  components: {
+    BooksSearch
+  }
+};
+</script>
