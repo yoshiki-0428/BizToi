2fd689db214b8823b90eb9950253969f9cfa4fa7
Yoshiki Ohashi <rrrtcdish2@gmail.com>
2019-09-18 00:29:02 +0900

    build.gradle.ktsの整理、DOMA->jooqを検討

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -1,6 +1,4 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-import org.jetbrains.kotlin.kapt3.base.Kapt.kapt
-import org.jetbrains.kotlin.resolve.calls.model.ResolvedCallArgument.DefaultArgument.arguments
 
 plugins {
 	id("org.springframework.boot") version "2.1.6.RELEASE"
@@ -9,7 +7,6 @@ plugins {
 
 	kotlin("jvm") version "1.3.50"
 	kotlin("plugin.spring") version "1.3.50"
-	kotlin("kapt") version "1.3.50"
 	java
 	idea
 }
@@ -45,36 +42,33 @@ repositories {
 	maven { url = uri("https://oss.sonatype.org/content/repositories/snapshots") }
 }
 
-val compileKotlin: KotlinCompile by tasks
-
-kapt {
-	useBuildCache = true
-	arguments {
-		arg("doma.resources.dir", compileKotlin.destinationDir)
-	}
-}
-
 extra["springCloudVersion"] = "Hoxton.M1"
 
 dependencies {
-
+	// Kotlin
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.1")
+
+	// Spring
 	implementation("org.springframework.boot:spring-boot-starter-webflux")
 	implementation("org.springframework.boot:spring-boot-starter-security")
-	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.1")
 	runtimeOnly("org.springframework.boot:spring-boot-devtools")
+
+	// db
+	runtimeOnly("mysql:mysql-connector-java")
+	implementation("org.springframework.boot:spring-boot-starter-jooq")
+
+	// Other
 	implementation("io.springfox:springfox-swagger2:2.9.2")
 	implementation("io.springfox:springfox-swagger-ui:2.9.2")
-	implementation("com.nimbusds:nimbus-jose-jwt:5.12")
 	implementation("com.amazonaws:aws-java-sdk-cognitoidp")
-	compile("org.seasar.doma.boot:doma-spring-boot-starter:1.1.1")
-	kapt("org.seasar.doma:doma:2.25.1")
-	implementation("org.seasar.doma:doma:2.25.1")
 	compileOnly("org.projectlombok:lombok")
-	runtimeOnly("mysql:mysql-connector-java")
 	annotationProcessor("org.projectlombok:lombok")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("com.nimbusds:nimbus-jose-jwt:5.12")
+
+	// Test tool
 	testImplementation("io.projectreactor:reactor-test")
 	testImplementation("org.springframework.boot:spring-boot-starter-test") {
 		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
@@ -88,20 +82,11 @@ dependencyManagement {
 	}
 }
 
-tasks.register("copyDomaResources", Sync::class){
-	from("src/main/resources")
-	into(compileKotlin.destinationDir)
-	include("doma.compile.config")
-	include("META-INF/**/*.sql")
-	include("META-INF/**/*.script")
-}
-
 tasks.withType<Test> {
 	useJUnitPlatform()
 }
 
 tasks.withType<KotlinCompile> {
-	dependsOn(tasks.getByName("copyDomaResources"))
 	kotlinOptions {
 		freeCompilerArgs = listOf("-Xjsr305=strict")
 		jvmTarget = "1.8"
