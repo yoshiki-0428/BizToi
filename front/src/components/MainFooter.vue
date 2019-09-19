19af1aa87be7fec213fa12cbddee56bb58bba0e6
ma-ryu <maryu.januar@gmail.com>
2019-09-19 22:23:37 +0900

    再）フッター作成

---
+++ front/src/components/MainFooter.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-footer padless>
+    <v-col
+            class="text-center"
+            cols="12"
+    >
+      <v-card-text>
+        <v-btn icon class=mx4 v-for="item in sns" :key="item.icon" :to="item.link">
+         <v-icon dark large v-text="item.icon"></v-icon>
+        </v-btn>
+      </v-card-text>
+      {{ new Date().getFullYear() }} — <strong>ビジトイ</strong>
+      <v-list>
+
+      </v-list>
+    </v-col>
+  </v-footer>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      sns: [
+        {
+          icon: "mdi-google",
+          link: ""
+        },
+        {
+          icon: "mdi-facebook-box",
+          link: ""
+        },
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .v-btn--icon{
+  text-align: center;
+    margin-right: 20px;
+  }
+</style>
